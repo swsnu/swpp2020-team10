28bab3d559be400f425fda14bf0d1a6b13473396
kslim99 <me9907@snu.ac.kr>
2020-11-07 11:15:51 +0900

    Modified App.js, created store, component directories, & actionTypes.js.

---
+++ frontend/src/store/actions/actionTypes.js
@@ -0,0 +1,12 @@
+export const GET_RECIPE = "GET_RECIPE";
+export const RATE_RECIPE = "RATE_RECIPE";
+export const GET_RECIPE_REVIEWS = "GET_REVIEWS";
+export const GET_REVIEW = "GET_REVIEW";
+export const ADD_REVIEW = "ADD_REVIEW";
+export const EDIT_REVIEW = "EDIT_REVIEW";
+export const DELETE_REVIEW = "DELETE_REVIEW";
+export const GET_REVIEW_COMMENTS = "GET_REVIEW_COMMENTS";
+export const GET_COMMENT = "GET_COMMENT";
+export const ADD_COMMENT = "ADD_COMMENT";
+export const EDIT_COMMENT = "EDIT_COMMENT";
+export const DELETE_COMMENT = "DELETE_COMMENT";
