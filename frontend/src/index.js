f024ac9a40d41ba32715c2d1e13c51606b4c687d
sc613 <sc613@snu.ac.kr>
2020-11-07 23:40:14 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux';
+import thunk from 'redux-thunk';
+
+import recipeReducer from './store/reducers/recipe';
+import reviewReducer from './store/reducers/review';
+import commentReducer from './store/reducers/comment';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const reducer = combineReducers({
+  recipe: recipeReducer,
+  review: reviewReducer,
+  comment: commentReducer,
+});
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
