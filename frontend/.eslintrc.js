cec3c83537b0188e427e3c4a22bd1c5925ead670
sc613 <sc613@snu.ac.kr>
2020-11-08 21:29:30 +0900

    Update .eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,40 +1,43 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true,
-        'jest': true // This is for jest
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
+    'jest': true // This is for jest
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended'
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1
+      }
     ],
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 12,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react'
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
 };
