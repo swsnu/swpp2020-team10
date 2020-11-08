0308c2989b7aca02c77315667a6fe2325eaa9a27
sc613 <sc613@snu.ac.kr>
2020-11-08 22:51:46 +0900

    Create user actions and reducers

---
+++ frontend/src/store/actions/user.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import * as actionTypes from './actionTypes';
+
+
+const authorize = (response_data) => {
+  return {
+    type: actionTypes.SIGN_IN,
+    id: response_data.user_id,
+    name: response_data.username,
+  };
+};
+
+export const signin = (request_data) => {
+  return dispatch => {
+    return axios.post('/api/user/signin/', request_data)
+      .then(response => {
+        dispatch(authorize(response.data));
+        return response;
+      })
+      .catch(response => response);
+  };
+};
+
+export const checkUserStatus = () => {
+  return dispatch => {
+    return axios.get('/api/user/status')
+      .then(response => {
+        if (response.status == 200) {
+          dispatch(authorize(response.data));
+        }
+      });
+  };
+};
+
+
+const unauthorize = () => {
+  return { type: actionTypes.SIGN_OUT };
+};
+
+export const signout = () => {
+  return dispatch => {
+    return axios.get('/api/user/signout/')
+      .then(() => {
+        dispatch(unauthorize());
+      });
+  };
+};
