7998c48d89459634d21a018f81bcb164fae3ff90
sc613 <sc613@snu.ac.kr>
2020-11-09 10:32:20 +0900

    Add fetch all recipes action

--- frontend/src/store/actions/actionTypes.js
+++ frontend/src/store/actions/actionTypes.js
@@ -3,6 +3,8 @@ export const SIGN_OUT = 'SIGN_OUT';
 
 export const GET_RECIPE = 'GET_RECIPE';
 export const RATE_RECIPE = 'RATE_RECIPE';
+export const FETCH_ALL_RECIPES = 'FETCH_ALL_RECIPES';
+
 export const GET_RECIPE_REVIEWS = 'GET_RECIPE_REVIEWS';
 export const GET_REVIEW = 'GET_REVIEW';
 export const ADD_REVIEW = 'ADD_REVIEW';
@@ -11,6 +13,7 @@ export const LIKE_REVIEW = 'LIKE_REVIEW';
 export const DISLIKE_REVIEW = 'DISLIKE_REVIEW';
 export const REPORT_REVIEW = 'GET_REVIEW';
 export const DELETE_REVIEW = 'GET_REVIEW';
+
 export const GET_REVIEW_COMMENTS = 'GET_REVIEW_COMMENTS';
 export const GET_COMMENT = 'GET_COMMENT';
 export const ADD_COMMENT = 'ADD_COMMENT';
