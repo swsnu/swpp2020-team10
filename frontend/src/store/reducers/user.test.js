f31d6fb356810968a8171eb404b564f96d42af04
sc613 <sc613@snu.ac.kr>
2020-11-14 16:38:02 +0900

    Add user redux tests

---
+++ frontend/src/store/reducers/user.test.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+import * as actionTypes from '../actions/actionTypes';
+import reducer from './user';
+
+
+describe('user reducer', () => {
+  it('SIGN_IN', () => {
+    reducer(undefined, {
+      type: actionTypes.SIGN_IN,
+      id: 1,
+      name: 'name 1',
+    });
+  });
+
+  it('SIGN_OUT', () => {
+    reducer(undefined, {
+      type: actionTypes.SIGN_OUT,
+    });
+  });
+
+  it('default', () => {
+    reducer(undefined, {});
+  });
+
+});
