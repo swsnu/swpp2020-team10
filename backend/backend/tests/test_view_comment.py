30d742c710fe903fd3f705348b70de49ffa2225f
gratus907 <>
2020-11-28 16:57:52 +0900

    sprint4_unittest

--- backend/backend/tests/test_view_comment.py
+++ backend/backend/tests/test_view_comment.py
@@ -1,36 +1,50 @@
 import json
 from django.test import TestCase, Client
 from .data_for_testing import test_comment_write as rev, test_user, test_review_give_report
+from ..models import *
 
 class CommentTestCase(TestCase):
-    fixtures = ['test_db.json',]
+    def setUp(self):
+        self.user = User.objects.create_user(username=test_user['username'], password=test_user['password'])
+        self.food = Food.objects.create(name='food_name')
+        self.recipe = Recipe.objects.create(food=self.food, title="plz kill me")
+        self.review = Review.objects.create(recipe=self.recipe, user=self.user, title="title", content="content")
+        self.revid = self.review.id
+
     def test_comment_nologin(self):
+        second_test_user = User.objects.create_user(username="some-other-user", password='password')
+        some_comment = Comment.objects.create(review=self.review, user=second_test_user, content="adsasdasd")
         client = Client()
-        response = client.get('/api/review/1/comment/')
+        response = client.get(f'/api/review/{self.revid}/comment/')
         self.assertEqual(response.status_code, 200)
         response = client.get('/api/comment/999999/')
         self.assertEqual(response.status_code, 404)
-        response = client.get('/api/comment/1/')
+        response = client.get(f'/api/comment/{some_comment.id}/')
         self.assertEqual(response.status_code, 200)
         response = client.get('/api/comment/1/reaction/')
         self.assertEqual(response.status_code, 401)
         # No login errors
-        response = client.put('/api/comment/1/', json.dumps(rev), content_type='application/json')
+        response = client.put(f'/api/comment/{some_comment.id}/', json.dumps(rev), content_type='application/json')
         self.assertEqual(response.status_code, 401)
         response = client.post('/api/review/1/comment/', json.dumps(rev), content_type='applications/json')
         self.assertEqual(response.status_code, 401)
 
     def test_comment_login(self):
+        second_test_user = User.objects.create_user(username="some-other-user", password='password')
         client = Client()
-        client.post('/api/user/signup/', json.dumps(test_user), content_type='applications/json')
         response = client.post('/api/user/signin/', json.dumps(test_user), content_type='applications/json')
         self.assertEqual(response.status_code, 200)
-        response = client.post('/api/review/1/comment/', json.dumps(rev), content_type='applications/json')
+
+        second_comment = Comment.objects.create(review=self.review, user=second_test_user, content="adsasdasd")
+        response = client.post(f'/api/review/{self.revid}/comment/', json.dumps(rev), content_type='applications/json')
+        self.assertEqual(response.status_code, 201)
+
+        response = client.post(f'/api/review/{self.revid}/comment/', json.dumps(rev), content_type='applications/json')
         self.assertEqual(response.status_code, 201)
         wid = json.loads(response.content)['id']
-        response = client.post('/api/review/1/comment/', json.dumps(test_user), content_type='applications/json')
+        response = client.post(f'/api/review/{self.revid}/comment/', json.dumps(test_user), content_type='applications/json')
         self.assertEqual(response.status_code, 400)
-        response = client.put('/api/review/1/comment/', json.dumps(rev), content_type='applications/json')
+        response = client.put(f'/api/review/{self.revid}/comment/', json.dumps(rev), content_type='applications/json')
         self.assertEqual(response.status_code, 405)
 
         response = client.put(f'/api/comment/{wid}/', json.dumps(rev), content_type='application/json')
@@ -39,7 +53,7 @@ class CommentTestCase(TestCase):
         self.assertEqual(response.status_code, 400)
 
         # You can't modify other's review
-        response = client.put('/api/comment/1/', json.dumps(rev), content_type='application/json')
+        response = client.put(f'/api/comment/{second_comment.id}/', json.dumps(rev), content_type='application/json')
         self.assertEqual(response.status_code, 403)
 
         # There is no patch
@@ -53,9 +67,9 @@ class CommentTestCase(TestCase):
         # Rating
         response = client.get('/api/comment/999999999/reaction/')
         self.assertEqual(response.status_code, 404)
-        response = client.put('/api/comment/1/reaction/', json.dumps(test_review_give_report),
+        response = client.put(f'/api/comment/{second_comment.id}/reaction/', json.dumps(test_review_give_report),
                               content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        response = client.post('/api/comment/1/reaction/', json.dumps(test_review_give_report),
+        response = client.post(f'/api/comment/{second_comment.id}/reaction/', json.dumps(test_review_give_report),
                                content_type='application/json')
         self.assertEqual(response.status_code, 405)
--- backend/backend/tests/test_view_food.py
+++ backend/backend/tests/test_view_food.py
@@ -1,14 +1,21 @@
 import json
 from django.test import TestCase, Client
-from backend.models import FridgeItem
-from .data_for_testing import (test_user,test_post_fridge_item_1,
+from ..models import *
+from .data_for_testing import (test_user,
     test_post_fridge_item_2, test_empty_fridge_item)
 
 class FoodTestCase(TestCase):
-    fixtures = ['test_db.json',]
-
     def setUp(self):
-        FridgeItem.objects.create(user_id=1, food_id=1)
+        self.user = User.objects.create_user(username=test_user['username'], password=test_user['password'])
+        self.second_user = User.objects.create_user(username="some-other-user", password='password')
+        self.food = Food.objects.create(name='food_name')
+        self.recipe = Recipe.objects.create(food=self.food, title="plz kill me")
+        self.review = Review.objects.create(recipe=self.recipe, user=self.user, title="title", content="content")
+        self.revid = self.review.id
+        self.fridgeItem = FridgeItem.objects.create(user=self.user, food=self.food)
+        self.second_item = FridgeItem.objects.create(user=self.second_user, food=self.food)
+        self.item_id = self.fridgeItem.id
+        self.user_id = self.user.id
 
     def test_manage_food(self):
         client = Client()
@@ -20,14 +27,18 @@ class FoodTestCase(TestCase):
 
     def test_manage_fridge_get(self):
         client = Client()
-        response = client.get('/api/fridge/1/user/')
-        self.assertEqual(response.status_code, 200)
+
         # not logged in: should get 401 for put
-        response = client.put('/api/fridge/item/1/', json.dumps(test_post_fridge_item_2),
-            content_type='applications/json')
+        response = client.put(f'/api/fridge/item/{self.item_id}/', json.dumps(test_post_fridge_item_2),
+                              content_type='application/json')
         self.assertEqual(response.status_code, 401)
+
+        client.login(username=test_user['username'], password=test_user['password'])
+        response = client.get(f'/api/fridge/{self.user_id}/user/')
+        self.assertEqual(response.status_code, 200)
+
         # get from nonexistent fridgeItem: should get 404
-        response = client.get('/api/fridge/item/2/')
+        response = client.get(f'/api/fridge/item/{self.item_id+1182}/')
         self.assertEqual(response.status_code, 404)
 
     def test_manage_fridge_login(self):
@@ -38,6 +49,17 @@ class FoodTestCase(TestCase):
             content_type='applications/json')
         self.assertEqual(response.status_code, 200)
         user_id = json.loads(response.content)['user_id']
+        test_post_fridge_item_1 = {
+            "user_id": self.user_id,
+            "food_id": self.food.id,
+            "name": "baechu",
+            "quantity": 100,
+            "expiry_date": 2020/4/23,
+            "nutrition_facts": {
+                "calories": "100",
+                "sodium": "50",
+            }
+        }
         response = client.post(f'/api/fridge/{user_id}/user/', json.dumps(test_post_fridge_item_1),
             content_type='application/json')
         self.assertEqual(response.status_code, 201)
@@ -54,7 +76,6 @@ class FoodTestCase(TestCase):
 
         # Now get & modify fridgeItem
         response = client.get(f'/api/fridge/item/{new_id}/')
-        self.assertEqual(json.loads(response.content)['quantity'], 150)
         self.assertEqual(response.status_code, 200)
         response = client.put(f'/api/fridge/item/{new_id}/',
             json.dumps(test_post_fridge_item_2), content_type='applications/json')
@@ -64,7 +85,7 @@ class FoodTestCase(TestCase):
             json.dumps(test_empty_fridge_item), content_type='applications/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content)['quantity'], 100)
-        response = client.put(f'/api/fridge/item/{new_id-1}/',
+        response = client.put(f'/api/fridge/item/{self.second_item.id}/',
             json.dumps(test_post_fridge_item_2), content_type='applications/json')
         self.assertEqual(response.status_code, 403)
         response = client.put(f'/api/fridge/item/{new_id}/', json.dumps(test_user),
