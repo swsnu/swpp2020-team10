d14f5d530b68d42f2f47701106bf18b48c9c46c2
sc613 <50579429+sc613@users.noreply.github.com>
2020-12-10 17:26:38 +0900

    Fix menu on the top & amend search (#47)
    * Update RecipeDetail.js (without rating feature)
    
    * Update RecipeDetail.js (remove redundant if stmts)
    
    * Fix search backend & update frontend
    
    * Temporarily remove RecipeDetail.js (resolve conflicts)
    
    * Restore wrong deletes
    
    * Upload ReviewDetail.js
    
    * Intermediate merge to frontend-revamp (#43)
    
    * poc of recommendation
    
    * model fix
    
    * fix recommendation according to model changes
    
    * constant
    
    * Sprint5 kslim (#40)
    
    * Added clearFridge, bug fixed in recipe & review detail page
    
    * I hate eslint
    
    * resolved conflict
    
    * Added Profiles for reviews and comments
    
    * ReviewDetail page now recieves one reaction per user
    
    Co-authored-by: sc613 <50579429+sc613@users.noreply.github.com>
    
    Co-authored-by: gratus907 <>
    Co-authored-by: kslim99 <me9907@snu.ac.kr>
    
    * Add rating incidence
    
    * Update RecipeSearchPage.js (minor improvement)
    
    * Update store (recipe, review, comment)
    
    * Update RecipeDetail.js (enable rating)
    
    * Rework ReviewDetail.js
    
    * Update admin.py
    
    * Fix DateTimeField issue in models.py
    
    * Update view_recipe.py (RecipeProfile)
    
    * Update view_comment.py (convert to dict properly)
    
    * Change review create url (frontend)
    
    * Update RecipeDetail.js
    
    * Upload misc.js (getFormattedDate, validateImageUrl)
    
    * Update ReviewDetail.js
    
    * Refactor review pages & enable review image
    
    * Add image_url field to Review model
    
    * Fix reportReview dispatching wrong action type
    
    * Update RecipeDetail.js (change tab appearance)
    
    * Add back to recipe button on review page
    
    * Add unit back to FridgeItem model
    
    * Change url to My Fridge (frontend)
    
    * Fix fridgeItem actions & reducers
    
    * Update reducers/review.js
    
    * Update ReviewTabs.js (WriteTab)
    
    * Update MyFridge.js & FoodCreate.js & FoodDetail.js
    
    * Update notifications (frontend)
    
    * Add recommendation (frontend)
    
    * Fix menu at the top
    
    * Update RecipeDetail.js & RecipeSearchPage.js
    
    Co-authored-by: kslim99 <me9907@snu.ac.kr>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,51 +6,84 @@ import {
   Redirect,
   Switch,
   useHistory,
+  Link,
 } from 'react-router-dom';
-import { Container, Menu } from 'semantic-ui-react';
+
+import { Container, Header, Icon, Menu } from 'semantic-ui-react';
 
 import { FrontPage } from './components/FrontPage';
 import { RecipeSearchPage } from './components/RecipeSearchPage';
 import { SignInPage } from './components/user/SignInPage';
 import { SignUpPage } from './components/user/SignUpPage';
-import MyFridge from './components/myfridge/MyFridge';
-import RecipeDetail from './components/RecipeDetail';
-import ReviewDetail from './components/ReviewDetail';
-import ReviewCreator from './components/ReviewCreator';
-import ReviewEditor from './components/ReviewEditor';
+import { MyFridge } from './components/myfridge/MyFridge';
+import { RecipeDetail } from './components/RecipeDetail';
+import { ReviewDetail } from './components/ReviewDetail';
+import { ReviewCreator } from './components/ReviewCreator';
+import { ReviewEditor } from './components/ReviewEditor';
 
 import * as actionCreators from './store/actions/index';
 
 
-const MenuBeforeSignIn = () => {
+const MenuBefore = () => {
   const history = useHistory();
 
   return (
-    <Menu borderless>
+    <Menu borderless fixed='top'>
       <Container text>
-        <Menu.Menu position='right'>
-          <Menu.Item
-            content='Sign up'
-            onClick={() => history.push('/signup')}
-          />
-          <Menu.Item
-            content='Sign in'
-            onClick={() => history.push('/signin')}
-          />
-        </Menu.Menu>
+        <Menu.Item>
+          <Link to='/'>
+            <Header color='blue'>
+              <Icon name='snowflake' />FRIDGE
+            </Header>
+          </Link>
+        </Menu.Item>
+        <Menu.Item
+          position='right'
+          content='Sign up'
+          onClick={() => history.push('/signup')}
+        />
+        <Menu.Item
+          content='Sign in'
+          onClick={() => history.push('/signin')}
+        />
       </Container>
     </Menu>
   );
 };
 
 
-function WrappedRoute({ component: Component, ...rest }) {
+const MenuAfter = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <Menu borderless fixed='top'>
+      <Container text>
+        <Menu.Item>
+          <Link to='/'>
+            <Header color='blue'>
+              <Icon name='snowflake' />FRIDGE
+            </Header>
+          </Link>
+        </Menu.Item>
+        <Menu.Item
+          position='right'
+          content='Sign out'
+          onClick={() => dispatch(actionCreators.signout())}
+        />
+      </Container>
+    </Menu>
+  );
+};
+
+
+function WrappedRoute({ menu: Menu, component: Component, ...rest }) {
   return (
     <Route
       {...rest}
       render={routeProps => (
         <>
-          <MenuBeforeSignIn />
+          <Menu />
+          <br /><br /><br /><br />
           <Component {...routeProps} />
         </>
       )}
@@ -60,8 +93,6 @@ function WrappedRoute({ component: Component, ...rest }) {
 
 
 function App() {
-  const dispatch = useDispatch();
-
   const userIsAuthorized = useSelector(state => state.user.isAuthorized);
 
   if (userIsAuthorized === null) {
@@ -74,10 +105,10 @@ function App() {
         <Switch>
           <Route exact path='/signin' component={SignInPage} />
           <Route exact path='/signup' component={SignUpPage} />
-          <WrappedRoute exact path='/search/:q' component={RecipeSearchPage} />
-          <WrappedRoute exact path='/recipe/:recipe_id' component={RecipeDetail} />
-          <WrappedRoute exact path='/review/:review_id' component={ReviewDetail} />
-          <WrappedRoute exact path='/' component={FrontPage} />
+          <WrappedRoute menu={MenuBefore} exact path='/search/:q' component={RecipeSearchPage} />
+          <WrappedRoute menu={MenuBefore} exact path='/recipe/:recipe_id' component={RecipeDetail} />
+          <WrappedRoute menu={MenuBefore} exact path='/review/:review_id' component={ReviewDetail} />
+          <WrappedRoute menu={MenuBefore} exact path='/' component={FrontPage} />
           <Redirect to='/' />
         </Switch>
       </Router>
@@ -86,27 +117,14 @@ function App() {
 
   return (
     <Router>
-      <Menu borderless>
-        <Container text>
-          <Menu.Menu position='right'>
-            <Menu.Item
-              content='Settings'
-            />
-            <Menu.Item
-              content='Sign out'
-              onClick={() => dispatch(actionCreators.signout())}
-            />
-          </Menu.Menu>
-        </Container>
-      </Menu>
       <Switch>
-        <Route exact path='/search/:q' component={RecipeSearchPage} />
-        <Route exact path='/fridge/' component={MyFridge} />
-        <Route exact path='/recipe/:recipe_id' component={RecipeDetail} />
-        <Route exact path='/recipe/:recipe_id/create-review' component={ReviewCreator} />
-        <Route exact path='/review/:review_id' component={ReviewDetail} />
-        <Route exact path='/review/:review_id/edit' component={ReviewEditor} />
-        <Route exact path='/' component={FrontPage} />
+        <WrappedRoute menu={MenuAfter} exact path='/search/:q' component={RecipeSearchPage} />
+        <WrappedRoute menu={MenuAfter} exact path='/fridge/' component={MyFridge} />
+        <WrappedRoute menu={MenuAfter} exact path='/recipe/:recipe_id' component={RecipeDetail} />
+        <WrappedRoute menu={MenuAfter} exact path='/recipe/:recipe_id/create-review' component={ReviewCreator} />
+        <WrappedRoute menu={MenuAfter} exact path='/review/:review_id' component={ReviewDetail} />
+        <WrappedRoute menu={MenuAfter} exact path='/review/:review_id/edit' component={ReviewEditor} />
+        <WrappedRoute menu={MenuAfter} exact path='/' component={FrontPage} />
         <Redirect to='/' />
       </Switch>
     </Router>
