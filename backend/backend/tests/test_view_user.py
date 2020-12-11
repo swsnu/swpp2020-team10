a2eec89a8f8441f07200b25f308682c1fc8fd4d2
gratus907 <>
2020-12-11 18:25:21 +0900

    backend testing

--- backend/backend/tests/test_view_user.py
+++ backend/backend/tests/test_view_user.py
@@ -1,29 +1,28 @@
 import json
 from django.test import TestCase, Client
-from .data_for_testing import test_review_write as rev, test_user, test_review_give_report, test_user_2
-from ..models import User, Recipe, Food, FridgeItem, Review
+from .data_for_testing import test_review_write as rev, test_user, test_user_2, t_data
+from backend.models import User, Recipe, FridgeItem, Review
 
 class UserTestCase(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username=test_user['username'], password=test_user['password'])
+        test_user_3 = {
+            "username" : "unit-tester-3",
+            "password" : "p3141592",
+            "email" : "email@email.com"
+        }
         client = Client()
-        response = client.post('/api/user/signup/', json.dumps(test_user), content_type='application/json')
-        self.food = Food.objects.create(name='food')
-        self.second_user = User.objects.create_user(username="some-other-user", password='password')
-        self.recipe = Recipe.objects.create(food=self.food, title="plz kill me")
-        self.review = Review.objects.create(recipe=self.recipe, user=self.user, title="title", content="content")
-        self.revid = self.review.id
-        self.fridgeItem = FridgeItem.objects.create(user=self.user, food=self.food)
-        self.second_item = FridgeItem.objects.create(user=self.second_user, food=self.food)
-        self.item_id = self.fridgeItem.id
-        self.user_id = self.user.id
+        response = client.post('/api/user/signup/', json.dumps(test_user_3), content_type='application/json')
+        self.t_data = t_data()
 
     def test_user_signup(self):
         client = Client()
         response = client.post('/api/user/signup/', json.dumps(test_user_2), content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 409)
         response = client.put('/api/user/signup/', json.dumps(test_user), content_type='application/json')
         self.assertEqual(response.status_code, 405)
+
+        response = client.post('/api/user/signup/', json.dumps(test_user), content_type='application/json')
+        self.assertEqual(response.status_code, 409)
 
     def test_user_signin(self):
         client = Client()
@@ -36,9 +35,11 @@ class UserTestCase(TestCase):
         response = client.get('/api/user/signout/')
         self.assertEqual(response.status_code, 204)
         response = client.post('/api/user/signin/',json.dumps(test_user_2), content_type='application/json')
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, 200)
         response = client.put('/api/user/signin/',json.dumps(test_user), content_type='application/json')
         self.assertEqual(response.status_code, 405)
+        response = client.post('/api/user/signin/',json.dumps({'username':'asd','password':'apasd'}), content_type='application/json')
+        self.assertEqual(response.status_code, 401)
 
     def test_user_status(self):
         client = Client()
@@ -69,13 +70,13 @@ class UserTestCase(TestCase):
         response = client.post('/api/user/signin/',json.dumps(test_user), content_type='application/json')
         # Setup : write 1 review, 1 comment
         # make Fridgeitem.
-        response = client.post(f'/api/review/{self.revid}/comment/', json.dumps(rev), content_type='applications/json')
+        response = client.post(f'/api/review/{self.t_data.revid}/comment/', json.dumps(rev), content_type='applications/json')
         self.assertEqual(response.status_code, 201)
-        FridgeItem.objects.create(user=self.user, food=self.food)
-        rid = json.loads(client.post(f'/api/recipe/{self.recipe.id}/review/', json.dumps(rev),
+        FridgeItem.objects.create(user=self.t_data.user, ingredient=self.t_data.ingredient)
+        rid = json.loads(client.post(f'/api/recipe/{self.t_data.recipe.id}/review/', json.dumps(rev),
                                      content_type='applications/json').content)['id']
         client.post(f'/api/review/{rid}/comment/', json.dumps(rev), content_type='applications/json')
-        response = client.get(f'/api/user/{self.user_id}/notification/')
+        response = client.get(f'/api/user/{self.t_data.user_id}/notification/')
         self.assertEqual(response.status_code, 200)
-        response = client.delete(f'/api/user/{self.user_id}/notification/')
+        response = client.delete(f'/api/user/{self.t_data.user_id}/notification/')
         self.assertEqual(response.status_code, 405)
--- backend/backend/views/view_recommendation.py
+++ backend/backend/views/view_recommendation.py
@@ -9,8 +9,6 @@ def recommendation(request):
     return JsonResponse(result, safe=False)
 
 def recommendation_react(request):
-    rec_ing_list = dict()
-
     if not request.user.is_authenticated:
         return HttpResponse("You are not logged in\n",status=401)
     if not request.method == 'POST':
@@ -26,31 +24,37 @@ def recommendation_react(request):
     for it in LabelPreference.objects.filter(user_id=request_user_id).all().values():
         my_label_preference[it['name']] = it['score']
     for it in IngredientPreference.objects.filter(user_id=request_user_id).all().values():
-        my_ing_preference[it['id']] = it['score']
-
-
+        my_ing_preference[it['name']] = it['score']
+    #print(f"ing pref {request_user_id} : {my_ing_preference}")
     target_recipe = Recipe.objects.filter(id=recipe_id).all().values()[0]
     # Label preference update
 
     for label in target_recipe['diet_labels']:
         if label not in my_label_preference:
+            LabelPreference.objects.create(user_id=request_user_id, name=label, score=reaction)
+            lab_norm += 1
             continue
         my_label_preference[label] += reaction
         lab_norm += my_label_preference[label] * 2 * reaction - 1
-        print(f"{my_label_preference[label]-reaction} => {my_label_preference[label]} : add {my_label_preference[label] * 2 * reaction - 1}")
+        #print(f"{my_label_preference[label]-reaction} => {my_label_preference[label]} : add {my_label_preference[label] * 2 * reaction - 1}")
         LabelPreference.objects.filter(user_id=request_user_id, name=label).update(score=my_label_preference[label])
 
     for label in target_recipe['health_labels']:
         if label not in my_label_preference:
+            LabelPreference.objects.create(user_id=request_user_id, name=label, score=reaction)
+            lab_norm += 1
             continue
         my_label_preference[label] += reaction
         lab_norm += my_label_preference[label] * 2 * reaction - 1
-        print(f"{my_label_preference[label]-reaction} => {my_label_preference[label]} : add {my_label_preference[label] * 2 * reaction - 1}")
+        #print(f"{my_label_preference[label]-reaction} => {my_label_preference[label]} : add {my_label_preference[label] * 2 * reaction - 1}")
         LabelPreference.objects.filter(user_id=request_user_id, name=label).update(score=my_label_preference[label])
 
     for it in IngredientIncidence.objects.filter(recipe_id=target_recipe['id']).values():
-        ing = Ingredient.objects.filter(id=it['ingredient_id']).all().values()[0]['id']
+        ing = Ingredient.objects.filter(id=it['ingredient_id']).all().values()[0]['name']
+        #print(f"Update pref {request_user_id} {Ingredient.objects.filter(id=it['ingredient_id']).all().values()[0]}")
         if ing not in my_ing_preference:
+            IngredientPreference.objects.create(user_id=request_user_id, name=ing, score=reaction)
+            lab_norm += 1
             continue
         my_ing_preference[ing] += reaction
         ing_norm += my_ing_preference[ing] * 2 * reaction - 1
@@ -59,7 +63,7 @@ def recommendation_react(request):
 
     Preference.objects.filter(user_id=request_user_id) \
         .update(label_norm=lab_norm, ingredient_norm=ing_norm)
-    print(my_label_preference)
-    print(my_ing_preference)
-    print(lab_norm, ing_norm)
+    #print(my_label_preference)
+    #print(my_ing_preference)
+    #print(lab_norm, ing_norm)
     return HttpResponse("Updated preferences as you reacted")
